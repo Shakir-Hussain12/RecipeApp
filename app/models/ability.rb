6d296a2406fa736e76e8b103f5c217cd9260d04e
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-25 01:50:50 +0500

    Update requested changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,11 @@
 class Ability
   include CanCan::Ability
-
   def initialize(user)
-    user ||= User.new # Guest user (not logged in)
-
+    user ||= User.new
     if user.admin?
-      can :manage, :all # Admin has full access to all resources
+      can :manage, :all
     else
-      can :read, Post # Regular users can read posts
-      can :create, Post # Regular users can create new posts
-      can :manage, Post, user_id: user.id # Regular users can manage their own posts
+      can :read, :all
       can :manage, Recipe, user_id: user.id
     end
   end
