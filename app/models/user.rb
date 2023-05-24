007ddbfacfea54bc65053b03fbe4a27e4a55c3d9
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-24 16:07:35 +0500

    Update validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }, allow_blank: false
          
 end
