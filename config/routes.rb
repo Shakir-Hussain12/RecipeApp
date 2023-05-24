15343ad762a02b48b64b96876a1854124654ea2e
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-05-24 15:45:22 +0500

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  authenticated :user do
+    root to: 'recipes#index', as: :authenticated_root
+  end
+
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
+
+  resources :recipes, only: [:index, :new, :create]
 end
