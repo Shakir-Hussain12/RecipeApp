ac563ff8cd56dc628793b5e9301ccd534c55d9b4
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-25 05:40:05 +0500

    updated food_controller and added new form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
+         
   has_many :foods
+
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :foods, only: %i[index new create delete]
 
   # Defines the root path route ("/")
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
+
 end
