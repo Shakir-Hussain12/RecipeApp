0455b9058ec44f2017c58274c9a596ea6ad05ef1
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-26 05:47:44 +0500

    Removed unwanted files

--- app/models/food_recipe.rb
+++
@@ -1,4 +0,0 @@
-class FoodRecipe < ApplicationRecord
-  belongs_to :food
-  belongs_to :recipe
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_000626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "food_recipes", force: :cascade do |t|
-    t.integer "quantity", default: 0
-    t.bigint "food_id", null: false
-    t.bigint "recipe_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_food_recipes_on_food_id"
-    t.index ["recipe_id"], name: "index_food_recipes_on_recipe_id"
-  end
-
   create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement"
-    t.decimal "price"
+    t.string "name", null: false
+    t.string "measurement", null: false
+    t.decimal "price", null: false
     t.integer "quantity", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -76,8 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_000626) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "food_recipes", "foods"
-  add_foreign_key "food_recipes", "recipes"
   add_foreign_key "foods", "users"
   add_foreign_key "public_recipes", "foods"
   add_foreign_key "public_recipes", "recipes"
