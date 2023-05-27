3dddcf48c1450e819615949fd242f773e1113f6d
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-27 11:33:01 +0500

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_061106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "food_recipes", force: :cascade do |t|
+    t.integer "quantity", default: 0
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_food_recipes_on_food_id"
+    t.index ["recipe_id"], name: "index_food_recipes_on_recipe_id"
+  end
+
   create_table "foods", force: :cascade do |t|
     t.string "name", null: false
     t.string "measurement", null: false
